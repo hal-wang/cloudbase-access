27a733d8281790a41a23e142acb8b1c2cae1ca11
hal-wang <hi@hal.wang>
2021-01-23 21:13:51 +0800

    add MapCreater

--- bin/cba-map.js
+++ bin/cba-map.js
@@ -1,42 +1,7 @@
 #!/usr/bin/env node
 /* eslint-disable @typescript-eslint/no-var-requires */
 
-const fs = require("fs");
-const linq = require("linq");
-const path = require("path");
+const MapCreater = require("../dist/Router/MapCreater").default;
 
 const cfPath = process.argv[2];
-const cFolder = path.join(process.cwd(), cfPath);
-if (
-  !cfPath ||
-  !fs.existsSync(cFolder) ||
-  !fs.lstatSync(cFolder).isDirectory()
-) {
-  throw new Error(
-    "please input controllers folder path, for example 'src/controllers'"
-  );
-}
-
-function readFilesFromFolder(folderRePath, result) {
-  const storageItems = linq
-    .from(fs.readdirSync(path.join(cFolder, folderRePath)))
-    .select((item) => path.join(folderRePath, item));
-  storageItems.forEach((storageItem) => {
-    const stat = fs.lstatSync(path.join(cFolder, storageItem));
-    if (stat.isDirectory()) {
-      readFilesFromFolder(storageItem, result);
-    } else if (
-      stat.isFile() &&
-      (storageItem.endsWith(".js") || storageItem.endsWith(".ts"))
-    ) {
-      result.push(storageItem);
-    }
-  });
-  return result;
-}
-
-const result = readFilesFromFolder("", []);
-fs.writeFileSync(
-  path.join(process.cwd(), "cba-map.json"),
-  JSON.stringify(result)
-);
+new MapCreater(cfPath).write();
