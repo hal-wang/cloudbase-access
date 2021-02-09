eeca304c7bfcd468e919c7776b7e6ab36a8a23ee
hal-wang <hi@hal.wang>
2021-02-09 11:47:40 +0800

    logout

--- demo/cba-todo/cba-todo-web/src/views/Todo/Header.vue
+++ demo/cba-todo/cba-todo-web/src/views/Todo/Header.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="header">
     <div class="logo">CBA-TODO</div>
+
     <a-space size="large">
       <a-button
         type="primary"
@@ -9,20 +10,39 @@
         size="large"
         @click="handleAddTodo"
       />
-      <a-button
-        type="danger"
-        shape="circle"
-        icon="logout"
-        size="large"
-        @click="handleLogout"
-      />
+
+      <a-popover :title="user._id" trigger="click">
+        <div slot="content">
+          <a-popconfirm
+            title="Are you sure you want to log out?"
+            ok-text="Yes"
+            cancel-text="No"
+            @confirm="handleLogout"
+          >
+            <a-button type="danger" block @click.stop=""> Logout </a-button>
+          </a-popconfirm>
+        </div>
+        <a-button
+          type="primary"
+          shape="circle"
+          icon="user"
+          size="large"
+          @click.stop=""
+        />
+      </a-popover>
     </a-space>
   </div>
 </template>
 
 <script lang="ts">
+import User from "@/models/User";
 import Vue from "vue";
 export default Vue.extend({
+  computed: {
+    user(): User {
+      return this.$store.state.user.user;
+    },
+  },
   methods: {
     handleAddTodo() {
       this.$emit("add");
