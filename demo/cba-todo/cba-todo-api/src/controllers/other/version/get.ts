4f57ba6b4fac7c5049a8593521420422fd095fe5
hal-wang <hi@hal.wang>
2021-02-11 16:11:06 +0800

    fix version err

--- demo/cba-todo/cba-todo-api/src/controllers/other/version/get.ts
+++ demo/cba-todo/cba-todo-api/src/controllers/other/version/get.ts
@@ -2,7 +2,7 @@ import { Action, HttpResult } from "@hal-wang/cloudbase-access";
 
 export default class extends Action {
   async do(): Promise<HttpResult> {
-    const config = <Record<string, unknown>>require("../../../../package.json");
+    const config = <Record<string, unknown>>require("../../../package.json");
 
     return this.ok({
       version: config.version,
