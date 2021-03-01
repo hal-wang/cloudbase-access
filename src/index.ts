0e8ad216ebab556fa0bbe4c7420b1ba4b1e2829a
hal-wang <hi@hal.wang>
2021-03-01 17:57:01 +0800

    HttpResultError

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,28 @@
 import HttpResult from "./HttpResult";
 import ErrorMessage from "./HttpResult/ErrorMessage";
 import HttpResultStruct from "./HttpResult/HttpResultStruct";
+import HttpResultError from "./HttpResult/HttpResultError";
+
 import Router from "./Router";
 import RequestParams from "./Router/RequestParams";
 import RequestMethod from "./Router/RequestMethod";
-import Action from "./Action";
+
 import Middleware from "./Middleware";
 import MiddlewareType from "./Middleware/MiddlewareType";
 import MiddlewareResult from "./Middleware/MiddlewareResult";
+
+import Action from "./Action";
 import Authority from "./Authority";
 import AppInstance from "./AppInstance";
 import DbHelper from "./DbHelper";
 import ApiDocs from "./ApiDocs";
+import ApiParam from "./ApiDocs/ApiParam";
 
 export {
   HttpResult,
   ErrorMessage,
   HttpResultStruct,
+  HttpResultError,
   Action,
   Router,
   RequestParams,
@@ -28,4 +34,5 @@ export {
   AppInstance,
   DbHelper,
   ApiDocs,
+  ApiParam,
 };
