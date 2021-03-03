60cb6ffb1c192b1a9782cce98cfe321fc4210809
hal-wang <hi@hal.wang>
2021-03-03 14:20:41 +0800

    note parser

--- src/ApiDocs/ApiDocsNoteParser.ts
+++ src/ApiDocs/ApiDocsNoteParser.ts
@@ -1,10 +1,74 @@
 import ApiDocs from ".";
-import Action from "../Action";
+import { readFileSync, writeFileSync } from "fs";
+import ApiDocsNoteParserStruct from "./ApiDocsNoteParserStruct";
+import path = require("path");
 
 export default class ApiDocsNoteParser {
-  constructor(private readonly file: string, private readonly action: Action) {}
+  constructor(private readonly file: string) {}
 
-  public get docs(): ApiDocs | undefined {
-    return;
+  public get docs(): ApiDocs | null {
+    const note = this.note;
+    if (!note) return null;
+    const parserResult = this.parser(note, 1);
+    console.log("parserResult", parserResult);
+    writeFileSync(
+      path.join(process.cwd(), "test.json"),
+      JSON.stringify(parserResult)
+    );
+    return null;
+  }
+
+  private get note(): string | null {
+    const content = readFileSync(this.file, "utf-8");
+    const regs = /(\/\*\*[\s]*\*[\s]*)(@action)([\s\S]*?\*\/)/gi.exec(content);
+    if (!regs || !regs.length) return null;
+
+    const note = regs[0]
+      .replace(/^[\s]*\*[\s]*$/gm, "")
+      .replace(/^[\s]*\*[\s]*/gm, "")
+      .replace(/[\s]*\/[\s]*$/, "")
+      .replace(/^[\s]*\/\*\*[\s]*/, "");
+    return note;
+  }
+
+  private parser(note: string, deep: number): ApiDocsNoteParserStruct[] | null {
+    const reg = this.getRegExp(deep);
+    const matchs = note.match(reg);
+    if (!matchs || !matchs.length) return null;
+    const splits = note.split(reg);
+    if (!splits || !splits.length) return null;
+    splits.splice(0, 1);
+    console.log("test2", matchs, splits);
+    if (matchs.length != splits.length) return null;
+
+    const result = [] as ApiDocsNoteParserStruct[];
+    for (let i = 0; i < matchs.length; i++) {
+      const header = matchs[i];
+      let title: string;
+      let value: string;
+      if (header.includes(" ")) {
+        const headerStrs = header.split(" ");
+        title = headerStrs[0];
+        value = header.replace(`${title} `, "");
+      } else {
+        title = header;
+        value = "";
+      }
+
+      result.push({
+        title: title,
+        value: value,
+        children: splits[i] ? this.parser(splits[i], deep + 1) : null,
+      });
+    }
+    return result;
+  }
+
+  private getRegExp(deep: number) {
+    let chars = "";
+    for (let i = 0; i < deep; i++) {
+      chars += "@";
+    }
+    return RegExp(`^${chars}[^@].*$`, "gm");
   }
 }
---
+++ test/docs/creater.test.ts
@@ -0,0 +1,21 @@
+import ApiDocsCreater from "../../src/ApiDocs/ApiDocsCreater";
+import ApiDocsParam from "../../src/ApiDocs/ApiDocsParam";
+
+const creater = new ApiDocsCreater("test/controllers", {
+  baseInputHeaders: <ApiDocsParam[]>[
+    {
+      name: "base-header",
+      desc: "this is a base header",
+      type: "string",
+    },
+  ],
+});
+
+test("api docs creater", async function () {
+  const docs = creater.docs;
+  expect(!!docs).toBe(true);
+});
+
+test("api docs write", async function () {
+  creater.write("./test.md");
+});
