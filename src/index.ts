3a3214fed6210b4dcc839637e8c166b5b24694b7
hal-wang <hi@hal.wang>
2021-03-04 16:34:23 +0800

    add ApiDocs to index

--- src/index.ts
+++ src/index.ts
@@ -11,12 +11,19 @@ import Middleware from "./Middleware";
 import MiddlewareType from "./Middleware/MiddlewareType";
 import MiddlewareResult from "./Middleware/MiddlewareResult";
 
+import ApiDocs from "./ApiDocs";
+import ApiDocsParam from "./ApiDocs/ApiDocsParam";
+import ApiDocsInputParams from "./ApiDocs/ApiDocsInputParams";
+import ApiDocsOutputParams from "./ApiDocs/ApiDocsOutputParams";
+import ApiDocsStateCode from "./ApiDocs/ApiDocsStateCode";
+import ApiDocsIOParams from "./ApiDocs/ApiDocsIOParams";
+import ApiDocsBasePart from "./ApiDocs/ApiDocsBasePart";
+import ApiDocsConfig from "./ApiDocs/ApiDocsConfig";
+
 import Action from "./Action";
 import Authority from "./Authority";
 import AppInstance from "./AppInstance";
 import DbHelper from "./DbHelper";
-import ApiDocs from "./ApiDocs";
-import ApiDocsParam from "./ApiDocs/ApiDocsParam";
 
 export {
   HttpResult,
@@ -35,4 +42,10 @@ export {
   DbHelper,
   ApiDocs,
   ApiDocsParam,
+  ApiDocsIOParams,
+  ApiDocsInputParams,
+  ApiDocsOutputParams,
+  ApiDocsStateCode,
+  ApiDocsBasePart,
+  ApiDocsConfig,
 };
