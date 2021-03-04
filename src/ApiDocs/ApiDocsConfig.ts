55b4aaf3b4bd5125f20cfb2b99629a03709202b0
hal-wang <hi@hal.wang>
2021-03-04 11:58:25 +0800

    add partsFromAuth

--- src/ApiDocs/ApiDocsConfig.ts
+++ src/ApiDocs/ApiDocsConfig.ts
@@ -2,6 +2,7 @@ import ApiDocsConfigPart from "./ApiDocsBasePart";
 
 export default interface ApiDocsConfig extends ApiDocsConfigPart {
   parts?: ApiDocsConfigPart[] | string[];
+  partsFromAuth?: boolean;
   title?: string;
   subtitle?: string;
 }
