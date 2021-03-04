07e45a5740a41cfa28334162c9b5efc4d7359eb7
hal-wang <hi@hal.wang>
2021-03-04 16:38:59 +0800

    change api docs config path

--- src/ApiDocs/ApiDocsConfig.ts
+++
@@ -1,8 +0,0 @@
-import ApiDocsConfigPart from "./ApiDocsBasePart";
-
-export default interface ApiDocsConfig extends ApiDocsConfigPart {
-  parts?: ApiDocsConfigPart[] | string[];
-  partsFromAuth?: boolean;
-  title?: string;
-  subtitle?: string;
-}
