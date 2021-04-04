804ea636405c7ab7c9ced55ac7e24aa7897d266f
hal-wang <hi@hal.wang>
2021-04-04 15:08:27 +0800

    todo collections

--- demo/cba-todo/cba-todo-api/src/lib/Collections.ts
+++ demo/cba-todo/cba-todo-api/src/lib/Collections.ts
@@ -1,21 +1,23 @@
 import Global from "./Global";
-import { CollectionReference } from "@cloudbase/database";
 import { AppInstance } from "@hal-wang/cloudbase-access";
+import { Database } from "@cloudbase/node-sdk";
 
 export default class Collections {
-  private static getCollection(collection: string): CollectionReference {
+  private static getCollection(
+    collection: string
+  ): Database.CollectionReference {
     let name;
     if (Global.isTest) name = `${collection}_test`;
     else name = collection;
 
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    return (AppInstance.instance.db as any).collection(name);
+    return AppInstance.instance.db.collection(name);
   }
 
-  static get user(): CollectionReference {
+  static get user(): Database.CollectionReference {
     return Collections.getCollection("cba-user");
   }
-  static get todo(): CollectionReference {
+  static get todo(): Database.CollectionReference {
     return Collections.getCollection("cba-todo");
   }
 }
