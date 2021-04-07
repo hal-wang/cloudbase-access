c13c01273485c7c44b2a613cba790b6e004196d1
hal-wang <hi@hal.wang>
2021-04-07 22:03:45 +0800

    remove map.test result

--- test/map.test.ts
+++ test/map.test.ts
@@ -1,8 +1,13 @@
 import MapCreater from "../src/Map/MapCreater";
+import * as fs from "fs";
 
 test("router test", async function () {
   const creater = new MapCreater("test/controllers");
   const map = creater.map;
   expect(!!map).toBe(true);
   creater.write();
+
+  const filePath = `${process.cwd()}/cba-map.json`;
+  expect(fs.existsSync(filePath)).toBe(true);
+  fs.rmSync(filePath);
 });
