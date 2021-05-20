c8547d27b9ee2b06b8069be26039158030f548af
hal-wang <hi@hal.wang>
2021-05-20 11:34:07 +0800

    feat: remove cba-map

--- bin/cba-map.js
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env node
-/* eslint-disable @typescript-eslint/no-var-requires */
-
-const MapCreater = require("../dist/Map/MapCreater").default;
-const path = require("path");
-const config = require(path.join(process.cwd(), "cba.config.json"));
-
-new MapCreater(path.join(config.target, config.router.controllers)).write();
